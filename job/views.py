43a9d62657509f5ea438041c445be0cf6d856a97
ChehAchraf <achrafchehboun@outlook.com>
2024-11-20 13:01:56 +0100

    add fix some issues with the details page

--- job/views.py
+++ job/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.paginator import Paginator
 # Create your views here.
 def job_list(reqeust):
     job_list = Job.objects.all()
@@ -8,5 +9,5 @@ def job_list(reqeust):
 
 def job_details(reqeust,id):
     job_detail = Job.objects.get(id=id)
-    context = {'job' : job_detail}
+    context = {'job' : job_detail, 'id' : id}
     return render(reqeust, 'job/job_details.html',context)
